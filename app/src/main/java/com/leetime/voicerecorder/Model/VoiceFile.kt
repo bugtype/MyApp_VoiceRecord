6c6afbf702e4f8e09378c6b204c2d099f87eb4d1
bugtype <bugtype.kr@gmail.com>
2019-03-07 17:52:29 +0900

    Added Voice File.

---
+++ app/src/main/java/com/leetime/voicerecorder/Model/VoiceFile.kt
@@ -0,0 +1,14 @@
+package com.leetime.voicerecorder.Model
+
+
+class VoiceFile  {
+
+    var path: String
+    var name: String
+    constructor(name: String, path: String){
+        this.path = path
+        this.name = name
+
+    }
+
+}
