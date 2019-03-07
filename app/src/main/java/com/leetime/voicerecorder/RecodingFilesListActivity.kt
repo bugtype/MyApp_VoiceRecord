db3057bb05dca4b9707d282638b2118b8ce4b034
bugtype <bugtype.kr@gmail.com>
2019-03-07 10:58:30 +0900

    Added Recording Files Activity

---
+++ app/src/main/java/com/leetime/voicerecorder/RecodingFilesListActivity.kt
@@ -0,0 +1,13 @@
+package com.leetime.voicerecorder
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RecodingFilesListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_record_files)
+
+    }
+}
